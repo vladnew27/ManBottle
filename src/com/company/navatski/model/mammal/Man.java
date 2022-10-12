e57486072230ea1c3c6526a6459ee9ab0c73e539
Vlad <vladnew27@gmail.com>
2022-10-12 21:12:51 +0200

    @Autowired     @Qualifier("cat2")

--- src/com/company/navatski/model/mammal/Man.java
+++ src/com/company/navatski/model/mammal/Man.java
@@ -3,11 +3,18 @@ package com.company.navatski.model.mammal;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+import java.lang.reflect.Type;
 
 @NoArgsConstructor
 public class Man extends Mammal implements Drinking {
+
     @Setter
     @Getter
+    @Autowired
+    @Qualifier("cat2")
     private Petable pet;
 
     public Man(String name, double stomachVolume, Petable pet) {
